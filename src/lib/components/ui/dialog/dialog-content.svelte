44b8dd55f084adbd53ead2497be6d1df3be66ce1
opensas <opensas@gmail.com>
2024-08-28 12:36:28 -0300

    hide Dialog close button

--- src/lib/components/ui/dialog/dialog-content.svelte
+++ src/lib/components/ui/dialog/dialog-content.svelte
@@ -6,9 +6,11 @@
 
 	import * as Dialog from './index.js';
 
-	type $$Props = DialogPrimitive.ContentProps;
+	type $$Props = DialogPrimitive.ContentProps & { hideCloseButton?: boolean };
 
 	let className: $$Props['class'] = undefined;
+
+	export let hideCloseButton = false;
 	export let transition: $$Props['transition'] = flyAndScale;
 	export let transitionConfig: $$Props['transitionConfig'] = {
 		duration: 200
@@ -28,11 +30,13 @@
 		{...$$restProps}
 	>
 		<slot />
-		<DialogPrimitive.Close
-			class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
-		>
-			<X class="h-4 w-4" />
-			<span class="sr-only">Close</span>
-		</DialogPrimitive.Close>
+		{#if !hideCloseButton}
+			<DialogPrimitive.Close
+				class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+			>
+				<X class="h-4 w-4" />
+				<span class="sr-only">Close</span>
+			</DialogPrimitive.Close>
+		{/if}
 	</DialogPrimitive.Content>
 </Dialog.Portal>
