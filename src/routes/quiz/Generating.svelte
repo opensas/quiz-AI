c08d58be9829b4978c5d822cee965fe482f0e12c
opensas <opensas@gmail.com>
2024-08-28 12:10:08 -0300

    create generating dialog

---
+++ src/routes/quiz/Generating.svelte
@@ -0,0 +1,23 @@
+<script>
+	import * as Dialog from '$lib/components/ui/dialog';
+
+	const { open = true } = $props();
+</script>
+
+<Dialog.Root closeOnEscape={false} closeOnOutsideClick={false} {open}>
+	<Dialog.Content>
+		<Dialog.Header>
+			<Dialog.Title>Generando preguntas</Dialog.Title>
+			<Dialog.Description class="text-nowrap">Esto puede tardar un rato...</Dialog.Description>
+		</Dialog.Header>
+		<div class="grid gap-4 self-center justify-self-center py-4">
+			<img
+				alt="IA generando preguntas"
+				height="480"
+				src="/img/generating/generating-quiz-01.webp"
+				title="IA generando preguntas"
+				width="640"
+			/>
+		</div>
+	</Dialog.Content>
+</Dialog.Root>
