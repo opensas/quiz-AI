dd69da87be6ff2dd61739629c252aa6031a9fe62
opensas <opensas@gmail.com>
2024-08-29 10:02:44 -0300

    animate resultado dialog

--- src/routes/quiz/dialogs/Resultado.svelte
+++ src/routes/quiz/dialogs/Resultado.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import type { Encuesta } from '$lib/types';
 
+	import Confetti from '$lib/components/encuesta/Confetti.svelte';
 	import { Button } from '$lib/components/ui/button';
 	import * as Dialog from '$lib/components/ui/dialog';
+
+	import { cubicOut } from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
 
 	type Props = {
 		open?: boolean;
@@ -14,15 +18,19 @@
 
 	const preguntas = $derived(encuesta.preguntas.length);
 	const correctas = $derived(encuesta.preguntas.filter((p) => p.respuesta === p.solucion).length);
-	const puntaje = $derived((correctas * 100) / preguntas);
+
+	let confetti = $state(false);
+	const puntaje = tweened(0, { duration: 1500, easing: cubicOut });
+	$effect(() => {
+		puntaje.set((correctas * 100) / preguntas).then(() => (confetti = true));
+	});
 
 	const resultados = encuesta.preguntas.map(({ titulo: preg, solucion: s, respuesta: resp }) => {
 		return `${preg}: ${resp} ` + (s === resp ? '✅' : `❌ => ${s} ✅`);
 	});
-
-	const round = (num: number) => (num % 1 !== 0 ? num.toFixed(2) : num);
-
 	console.log({ resultados });
+
+	const round = (num: number) => Math.round(num * 100) / 100;
 </script>
 
 <Dialog.Root closeOnEscape={false} closeOnOutsideClick={false} {open}>
@@ -31,15 +39,18 @@
 			<div class="grid gap-2">
 				<div class="text text-5xl font-black">Felicitaciones</div>
 				<div class="text-base text-muted-foreground">
-					Acertaste {correctas} de {preguntas} preguntas y se sacaste un
+					Acertaste {correctas} de {preguntas} preguntas y te sacaste un
 				</div>
 			</div>
 
 			<div class="grid animate-shake gap-2">
-				<div class="text-8xl font-bold tracking-tighter">{round(puntaje)}%</div>
+				<div class="text-8xl font-bold tracking-tighter">{round($puntaje)}%</div>
 				<div class="text-sm uppercase text-muted-foreground">Puntaje</div>
 			</div>
 		</div>
+
+		<Confetti infinite show={confetti} />
+
 		<Dialog.Close class="focus:outline-none">
 			<Button class="--focus:outline-none w-full" type="submit" onclick={onclose}>
 				Volver a jugar
